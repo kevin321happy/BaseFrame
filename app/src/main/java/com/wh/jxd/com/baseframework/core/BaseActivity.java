afc68a5a1d411c6464154525d1d1253af84a1fa6
kevin321 <kevin321vip@126.com>
2018-01-31 20:17:21 +0800

    项目分包

---
+++ app/src/main/java/com/wh/jxd/com/baseframework/core/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.wh.jxd.com.baseframework.core;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kevin321vip on 2018/1/31.
+ * BaseActivity
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
