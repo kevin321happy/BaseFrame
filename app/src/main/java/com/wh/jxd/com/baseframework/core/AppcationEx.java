85eb28131922073481b319ed49962f1aa8f5c502
kevin321 <kevin321vip@126.com>
2018-02-02 18:04:12 +0800

    应用程序初始化优化

--- app/src/main/java/com/wh/jxd/com/baseframework/core/AppcationEx.java
+++ app/src/main/java/com/wh/jxd/com/baseframework/core/AppcationEx.java
@@ -2,6 +2,8 @@ package com.wh.jxd.com.baseframework.core;
 
 import android.app.Application;
 
+import com.wh.jxd.com.baseframework.service.InitializeService;
+
 /**
  * Created by kevin321vip on 2018/2/2.
  */
@@ -10,7 +12,7 @@ public class AppcationEx extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        //初始化App状态跟踪
-        AppStatuesTracker.init(this);
+        //在服务中统一做初始化工作
+        InitializeService.start(this);
     }
 }
---
+++ app/src/main/java/com/wh/jxd/com/baseframework/service/InitializeService.java
@@ -0,0 +1,50 @@
+package com.wh.jxd.com.baseframework.service;
+
+import android.app.Application;
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+import com.wh.jxd.com.baseframework.core.AppStatuesTracker;
+
+/**
+ * Created by kevin321vip on 2018/2/2.
+ */
+
+public class InitializeService extends IntentService {
+    private static Context mContext;
+    private static final String ACTION_INIT_WHEN_APP_CREATE = "com.wh.jxd.com.refactorqm.service.action.INIT";
+
+    /**
+     * Creates an IntentService.  Invoked by your subclass's constructor.
+     */
+    public InitializeService() {
+        super("InitializeService");
+    }
+
+    public static void start(Context context) {
+        mContext = context;
+        Intent intent = new Intent(context, InitializeService.class);
+        intent.setAction(ACTION_INIT_WHEN_APP_CREATE);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if (intent != null) {
+            String action = intent.getAction();
+            if (ACTION_INIT_WHEN_APP_CREATE.equals(action)) {
+                performInit();
+            }
+        }
+    }
+
+    /**
+     * 应用启动初始化
+     */
+    private void performInit() {
+        //初始化App状态跟踪
+        AppStatuesTracker.init((Application) mContext);
+    }
+}
