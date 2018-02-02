862661f4840c8e85baa9f625ebabc29d791a46a3
kevin321 <kevin321vip@126.com>
2018-02-02 12:02:44 +0800

    updata

--- app/src/main/java/com/wh/jxd/com/baseframework/core/AppStatuesTracker.java
+++ app/src/main/java/com/wh/jxd/com/baseframework/core/AppStatuesTracker.java
@@ -3,6 +3,7 @@ package com.wh.jxd.com.baseframework.core;
 import android.app.Activity;
 import android.app.Application;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Created by kevin321vip on 2018/1/31.
@@ -14,10 +15,10 @@ public class AppStatuesTracker implements Application.ActivityLifecycleCallbacks
     private int mActivityCount;
     private Application mApplication;
     private static AppStatuesTracker mAppStatuesTracker;
+    private String TAG="tracker";
 
     /**
      * 构造方法传人Application
-     *
      * @param application
      */
     private AppStatuesTracker(Application application) {
@@ -29,49 +30,47 @@ public class AppStatuesTracker implements Application.ActivityLifecycleCallbacks
     public static void init(Application application) {
         mAppStatuesTracker = new AppStatuesTracker(application);
     }
-
     /**
      * 单例获取对象
      * @param activity
      * @param savedInstanceState
      */
-
-
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
-
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityCreated");
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityStarted");
         //走一次start就添加一次Activtiy
         mActivityCount++;
-
     }
 
     @Override
     public void onActivityResumed(Activity activity) {
-
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityResumed");
     }
 
     @Override
     public void onActivityPaused(Activity activity) {
-
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityPaused");
     }
 
     @Override
     public void onActivityStopped(Activity activity) {
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityStopped");
         mActivityCount--;
 
     }
 
     @Override
     public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivitySaveInstanceState");
     }
 
     @Override
     public void onActivityDestroyed(Activity activity) {
-
+        Log.d(TAG,"当前的Activity:"+activity.toString()+",onActivityDestroyed");
     }
 }
