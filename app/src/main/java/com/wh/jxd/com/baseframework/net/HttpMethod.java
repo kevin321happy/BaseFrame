a51388e9c11c433e0c3f7a2fa8713deebcd688f9
kevin321 <kevin321vip@126.com>
2018-02-08 17:37:51 +0800

    updata

---
+++ app/src/main/java/com/wh/jxd/com/baseframework/net/HttpMethod.java
@@ -0,0 +1,14 @@
+package com.wh.jxd.com.baseframework.net;
+
+/**
+ * Created by kevin321vip on 2018/2/7.
+ * 请求方法的枚举类
+ */
+
+public enum HttpMethod {
+    GET,
+    PUT,
+    POST,
+    DELETE,
+    PUT_ROW
+}
---
+++ app/src/main/java/com/wh/jxd/com/baseframework/net/RestService.java
@@ -0,0 +1,57 @@
+package com.wh.jxd.com.baseframework.net;
+
+import java.util.WeakHashMap;
+
+import io.reactivex.Observer;
+import okhttp3.RequestBody;
+import retrofit2.http.DELETE;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
+
+/**
+ * Created by kevin321vip on 2018/2/7.
+ * 网络请求的Service
+ */
+
+public interface RestService {
+    /**
+     * get请求
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    @GET
+    Observer<String> get(@Url String url, @QueryMap WeakHashMap<String, Object> params);
+
+    /**
+     * post方法
+     *
+     * @param url
+     * @param params
+     * @return
+     */
+    @FormUrlEncoded
+
+    @POST
+    Observer<String> post(@Url String url, @QueryMap WeakHashMap<String, Object> params);
+
+    /**
+     * postBody参数
+     */
+    @POST
+    Observer<String> post(@Url String url, RequestBody body);
+
+    /**
+     * put方法
+     */
+    @PUT
+    Observer<String> put(@Url String url, WeakHashMap<String, Object> params);
+
+    @DELETE
+    Observer<String> delete(@Url String url, WeakHashMap<String, Object> params);
+}
